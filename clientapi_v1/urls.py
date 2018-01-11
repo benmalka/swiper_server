0e77ef7ca3984e19de4263bd7b6725abfff27847
Ben Malka <ben2000567@gmail.com>
2018-01-11 18:21:42 +0200

    minor change in urls

--- clientapi_v1/urls.py
+++ clientapi_v1/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
         url(r'^add_user/$', views.add_user, name='add_user'),
         url(r'^category/$', views.category_list, name='category_list'),
         url(r'^items/$', views.items_list, name='items_list'),
-        url(r'^get_items/$', views.get_items, name='get_items'),
-        url(r'^like_item/$', views.like_item, name='like_item'),
-        url(r'^get_liked_items/$', views.get_liked_items, name='get_liked_items'),
-        url(r'^delete_liked_item/$', views.delete_liked_item, name='delete_liked_item'),
+        url(r'^get_items$', views.get_items, name='get_items'),
+        url(r'^like_item$', views.like_item, name='like_item'),
+        url(r'^get_liked_items$', views.get_liked_items, name='get_liked_items'),
+        url(r'^delete_liked_item$', views.delete_liked_item, name='delete_liked_item'),
 ]
