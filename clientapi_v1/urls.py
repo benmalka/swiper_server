1fc095a13f231a3f817e83db532cefb9fa56b082
matanr1 <matanr1@gmail.com>
2018-01-14 09:46:34 +0200

    remove slash from url

--- clientapi_v1/urls.py
+++ clientapi_v1/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 
 urlpatterns = [
-        url(r'^users/$', views.users_list, name='users_list'),
-        url(r'^add_user/$', views.add_user, name='add_user'),
-        url(r'^category/$', views.category_list, name='category_list'),
-        url(r'^items/$', views.items_list, name='items_list'),
+        url(r'^users$', views.users_list, name='users_list'),
+        url(r'^add_user$', views.add_user, name='add_user'),
+        url(r'^category$', views.category_list, name='category_list'),
+        url(r'^items$', views.items_list, name='items_list'),
         url(r'^get_items$', views.get_items, name='get_items'),
         url(r'^like_item$', views.like_item, name='like_item'),
         url(r'^get_liked_items$', views.get_liked_items, name='get_liked_items'),
